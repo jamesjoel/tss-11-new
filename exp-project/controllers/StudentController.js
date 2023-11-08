253e580a6ff1c1a27487671171f8f7267174ac8a
jamesjoel <jamesjoelj@gmail.com>
2023-11-08 13:32:51 +0530

    delete in express

--- exp-project/controllers/StudentController.js
+++ exp-project/controllers/StudentController.js
@@ -1,11 +1,26 @@
 const routes = require("express").Router();
 const Student = require("../models/Student")
+
 routes.get("/", (req, res)=>{
     res.render("pages/student");
 })
 
-routes.post("/save", (req, res)=>{
-    Student.create(req.body);
-    res.redirect("/");
+routes.post("/save", async (req, res)=>{
+    await Student.create(req.body);
+    res.redirect("/student/list");
 })
+
+routes.get("/list", async(req, res)=>{
+    let result = await Student.find();
+    let pagedata = { stu : result };
+    res.render("pages/student-list", pagedata);
+})
+
+routes.get("/delete/:a", async (req, res)=>{
+    // console.log(req.params);
+    let x = req.params.a;
+    await Student.deleteMany({ _id : x });
+    res.redirect("/student/list");
+})
+
 module.exports = routes;
--- exp-project/controllers/TeacherController.js
+++ exp-project/controllers/TeacherController.js
@@ -4,9 +4,15 @@ const Teacher = require("../models/Teacher");
 routes.get("/", (req, res)=>{
     res.render("pages/teacher");
 })
-routes.post("/save", (req, res)=>{
-    Teacher.create(req.body);
-    res.redirect("/");
+routes.post("/save", async (req, res)=>{
+    await Teacher.create(req.body);
+    res.redirect("/teacher/list");
+})
+
+routes.get("/list", async (req, res)=>{
+    let result = await Teacher.find();
+    let pagedata = { result : result };
+    res.render("pages/teacher-list", pagedata);
 })
 
 

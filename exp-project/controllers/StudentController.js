6397e916de59520431d7407c0c6def667f37766c
jamesjoel <jamesjoelj@gmail.com>
2023-11-10 13:18:01 +0530

    edit

--- exp-project/controllers/StudentController.js
+++ exp-project/controllers/StudentController.js
@@ -23,4 +23,17 @@ routes.get("/delete/:a", async (req, res)=>{
     res.redirect("/student/list");
 })
 
+routes.get("/edit/:x", async(req, res)=>{
+    let a = req.params.x;
+    let result = await Student.find({ _id : a });
+    let pagedata = {stu : result[0]};
+    res.render("pages/student-edit", pagedata)
+})
+
+routes.post("/update/:id", async(req, res)=>{
+    let id = req.params.id;
+    await Student.updateMany({_id : id }, req.body);
+    res.redirect("/student/list");
+})
+
 module.exports = routes;

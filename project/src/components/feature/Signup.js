ae5979e39b5bd06d578c1a20431dfc3cba6a1dfc
jamesjoel <jamesjoelj@gmail.com>
2023-12-12 13:00:37 +0530

    validation in react yup

--- project/src/components/feature/Signup.js
+++ project/src/components/feature/Signup.js
@@ -1,10 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import { useFormik } from 'formik';
+import * as YUP from 'yup'
+
+
+const SignupSchema = YUP.object({
+    name : YUP.string().required("Insert Your Full Name"),
+    username : YUP.string().required("Insert Your Username/Email"),
+    password : YUP.string().required("Insert Your Password"),
+    repassword : YUP.string().required("Insert Your Re-Password"),
+    contact : YUP.string().required("Insert Your Contact"),
+    address : YUP.string().required("Insert Your Address"),
+    state : YUP.string().required("Select Your State"),
+    city : YUP.string().required("Select Your City"),
+    gender : YUP.string().required("Select Your Gender"),
+});
+
+
 
 const Signup = () => {
 
     let signupForm = useFormik({ 
+        validationSchema : SignupSchema,
         initialValues : {
             name : "",
             username : "",
@@ -62,54 +79,83 @@ const Signup = () => {
                     <div className='card-body'>
                         <div className='my-3'>
                             <label>Full Name</label>
-                            <input type='text' onChange={signupForm.handleChange} name='name' className='form-control' />
+                            <input type='text' onChange={signupForm.handleChange} name='name' className={'form-control ' + (signupForm.errors.name && signupForm.touched.name ? 'is-invalid' : '')} />
+                            {
+                                signupForm.errors.name && signupForm.touched.name ? <small className='text-danger'>{signupForm.errors.name}</small> : ''
+                            }
+                            
                         </div>
                         <div className='my-3'>
                             <label>Email/Username</label>
-                            <input type='text' onChange={signupForm.handleChange} name='username' className='form-control' />
+                            <input type='text' onChange={signupForm.handleChange} name='username' className={'form-control '+ (signupForm.errors.username && signupForm.touched.username ? 'is-invalid' : '')} />
+                            {
+                                signupForm.errors.username && signupForm.touched.username ? <small className='text-danger'>{signupForm.errors.username}</small> : ''
+                            }
                         </div>
                         <div className='my-3'>
                             <label>Password</label>
-                            <input type='password' onChange={signupForm.handleChange} name='password' className='form-control' />
+                            <input type='password' onChange={signupForm.handleChange} name='password' className={'form-control ' + (signupForm.errors.password && signupForm.touched.password ? 'is-invalid' : '')} />
+                            {
+                                signupForm.errors.password && signupForm.touched.password ? <small className='text-danger'>{signupForm.errors.password}</small> : ''
+                            }
                         </div>
                         <div className='my-3'>
                             <label>Re-Password</label>
-                            <input type='password' onChange={signupForm.handleChange} name='repassword' className='form-control' />
+                            <input type='password' onChange={signupForm.handleChange} name='repassword' className={'form-control ' + (signupForm.errors.repassword && signupForm.touched.repassword ? 'is-invalid' : '')} />
+                            {
+                                signupForm.errors.repassword && signupForm.touched.repassword ? <small className='text-danger'>{signupForm.errors.repassword}</small> : ''
+                            }
                         </div>
                         <div className='my-3'>
                             <label>Address</label>
-                            <textarea className='form-control' onChange={signupForm.handleChange} name='address' ></textarea>
+                            <textarea className={'form-control ' + (signupForm.errors.address && signupForm.touched.address ? 'is-invalid' : '')} onChange={signupForm.handleChange} name='address' ></textarea>
+                            {
+                                signupForm.errors.address && signupForm.touched.address ? <small className='text-danger'>{signupForm.errors.address}</small> : ''
+                            }
                         </div>
                         <div className='my-3'>
                             <label>State</label>
-                            <select onChange={(e)=>{ getCity(e); signupForm.handleChange(e) }} name='state' className='form-control'>
+                            <select onChange={(e)=>{ getCity(e); signupForm.handleChange(e) }} name='state' className={'form-control ' + (signupForm.errors.state && signupForm.touched.state ? 'is-invalid' : '')}>
                                 <option>Select</option>
                                 {
                                     state.map(value=><option>{value}</option>)
                                 }
                             </select>
+                            {
+                                signupForm.errors.state && signupForm.touched.state ? <small className='text-danger'>{signupForm.errors.state}</small> : ''
+                            }
                         </div>
 
                         <div className='my-3'>
                             <label>City</label>
-                            <select className='form-control' onChange={signupForm.handleChange} name='city'>
+                            <select className={'form-control ' + (signupForm.errors.city && signupForm.touched.city ? 'is-invalid' : '')} onChange={signupForm.handleChange} name='city'>
                                 <option>Select</option>
                                 {
                                     city.map(value=><option>{value.name}</option>)
                                 }
                             </select>
+                            {
+                                signupForm.errors.city && signupForm.touched.city ? <small className='text-danger'>{signupForm.errors.city}</small> : ''
+                            }
                         </div>
                         <div className='my-3'>
                             <label>Gender</label><br />
                             Male&nbsp;&nbsp;&nbsp;<input type='radio' onChange={signupForm.handleChange} name='gender' value="male"/>
                             &nbsp;&nbsp;&nbsp;
                             Female&nbsp;&nbsp;&nbsp;<input type='radio' onChange={signupForm.handleChange} name='gender' value="female"/>
+                            <br />
+                            {
+                                signupForm.errors.gender && signupForm.touched.gender ? <small className='text-danger'>{signupForm.errors.gender}</small> : ''
+                            }
                         </div>
                         <div className='my-3'>
                             <label>Contact</label>
                             
 
-                                    <input type='text' onChange={signupForm.handleChange} name='contact' placeholder='Contact' className='form-control' />
+                                    <input type='text' onChange={signupForm.handleChange} name='contact' placeholder='Contact' className={'form-control ' + (signupForm.errors.contact && signupForm.touched.contact ? 'is-invalid' : '')} />
+                                    {
+                                signupForm.errors.contact && signupForm.touched.contact ? <small className='text-danger'>{signupForm.errors.contact}</small> : ''
+                            }
                             
                         </div>
                     </div>

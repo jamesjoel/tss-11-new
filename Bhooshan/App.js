26dafa29a4f2604ca5effc666edc139651e848d9
Bhooshanpandre <bhooshanpandre200@gmail.com>
2024-02-15 17:02:49 +0530

    check

---
+++ Bhooshan/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import AllRoutes from './AllRoutes/AllRoutes'
+
+const App = () => {
+  return (
+    <>
+	{/* <div className="search-area">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-12">
+					<span className="close-btn"><i className="fas fa-window-close"></i></span>
+					<div className="search-bar">
+						<div className="search-bar-tablecell">
+							<h3>Search For:</h3>
+							<input type="text" placeholder="Keywords"/>
+							<button type="submit">Search <i className="fas fa-search"></i></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> */}
+	{/* <div className="abt-section mb-150">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-6 col-md-12">
+					<div className="abt-bg">
+						<a href="https://www.youtube.com/watch?v=DBLlFWYcIGQ" className="video-play-btn popup-youtube"><i className="fas fa-play"></i></a>
+					</div>
+				</div>
+				<div className="col-lg-6 col-md-12">
+					<div className="abt-text">
+						<p className="top-sub">Since Year 1999</p>
+						<h2>We are <span className="orange-text">Fruitkha</span></h2>
+						<p>Etiam vulputate ut augue vel sodales. In sollicitudin neque et massa porttitor vestibulum ac vel nisi. Vestibulum placerat eget dolor sit amet posuere. In ut dolor aliquet, aliquet sapien sed, interdum velit. Nam eu molestie lorem.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente facilis illo repellat veritatis minus, et labore minima mollitia qui ducimus.</p>
+						<a href="about.html" className="boxed-btn mt-4">know more</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> */}
+	 
+   <AllRoutes/>
+  
+    </>
+  )
+}
+
+export default App
